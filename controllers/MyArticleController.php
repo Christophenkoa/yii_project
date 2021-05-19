25c0dd3249f4652b09f71f4883bfc5d36a6d1cd8
Nkoa Christophe <nkoachristophe@gmail.com>
2021-05-19 18:07:15 +0100

    starting poing

---
+++ controllers/MyArticleController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+class MyArticleController extends \yii\web\Controller {
+    
+    public function actionHelloWorld() {
+        echo "Hello world";
+    }
+}
