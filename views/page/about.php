52b1c3030a4c53992f279a0b9538887982aad8f4
Nkoa Christophe <nkoachristophe@gmail.com>
2021-05-20 23:23:54 +0100

    yii view

---
+++ views/page/about.php
@@ -0,0 +1,11 @@
+<?php
+    $this->title = 'Page - About us';
+?>
+
+<h1> Hello Page</h1>
+
+<?php echo $key; ?><br>
+
+<?php echo $this->context->id; ?>
+
+<?php // echo $this->render('../site/about'); ?>
